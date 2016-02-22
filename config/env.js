1f30311ae72136a484f19f0e222a4f496d7b8f9e
Mahan <eng.mahan.hazrati@gmail.com>
2016-02-23 01:11:40 +0700

    redis session || scaling

--- config/env.js
+++ config/env.js
@@ -1,7 +1,39 @@
 module.exports = function(app, express){
     var path = require('path');
+    var favicon = require('serve-favicon');
+    var logger = require('morgan');
+    var cookieParser = require('cookie-parser');
+    var bodyParser = require('body-parser');
+
+
+    // session on redis
+    var redis   = require("redis");
+    var session = require('express-session');
+    var redisStore = require('connect-redis')(session);
+    var client  = redis.createClient();
+
+    app.use(session({
+        secret: 'redis-secret',
+        // create new redis store.
+        store: new redisStore({ 
+            host: '127.0.0.1', 
+            port: 6379, 
+            client: client,
+            ttl :  2600000
+        }),
+        saveUninitialized: false,
+        resave: false
+    }));
 
     // view engine setup
     app.set('views', path.join(__dirname, '../views'));
     app.set('view engine', 'hbs')
+
+
+    app.use(favicon(path.join(__dirname, '../public/assets/images/favicon.ico')));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, '../public')));
 };
