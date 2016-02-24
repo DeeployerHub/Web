ea0d1b9965e98b835a602d51ddacbeb30b633fe3
Mahan <eng.mahan.hazrati@gmail.com>
2016-02-25 01:11:21 +0700

    bower and gulp config

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# d-web
+## preparation
+* you need to install the essential dependencies
+```
+sudo npm install gulp -g
+sudo npm install bower -g
+```
+
+* and then setup the environment
+```
+# this script runs the bower install and gulp
+npm run-script setup
+```
+
+## gulp modules
+* compile the assets from ./assets directory to public directory
+```
+gulp
+# watch on assets
+gulp assets:watch
+```
--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,48 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var dir = {
+    assets: {
+        source: './assets',
+        dest: './public/assets',
+        bower: './bower_components'
+    },
+}
 
 
-gulp.task('default', ['sass', 'css-copy']);
+gulp.task('default', ['assets', 'bower-assets']);
 
-gulp.task('css-copy', function () {
+gulp.task('bower-assets', function () {
+    var copyList = [
+        'jquery',
+        'bootstrap',
+        'owl.carousel',
+        'jquery.easing',
+    ];
+
+    for (var i in copyList) {
+        gulp.src(dir.assets.bower + '/' + copyList[i] + '/**', {base: dir.assets.bower + '/' + copyList[i]})
+            .pipe(gulp.dest(dir.assets.dest + '/bower/' + copyList[i] + '/'));
+    }
+
+});
+
+gulp.task('assets', function () {
     var copyList = ['css', 'images', 'js', 'fonts'];
 
     for (var i in copyList) {
-        gulp.src('./assets/' + copyList[i] + '/**', {base: './assets/' + copyList[i]})
-            .pipe(gulp.dest('./public/assets/' + copyList[i] + '/'));
+        gulp.src(dir.assets.source + '/' + copyList[i] + '/**', {base: dir.assets.source + '/' + copyList[i]})
+            .pipe(gulp.dest(dir.assets.dest + '/' + copyList[i] + '/'));
     }
-});
-gulp.task('sass', function () {
-    return gulp.src('./assets/sass/**/*.scss')
+
+    gulp.src(dir.assets.source + '/sass/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./public/assets/css'));
+        .pipe(gulp.dest(dir.assets.dest + '/css'))
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass', 'css-copy']);
+gulp.task('assets:watch', function () {
+    gulp.watch(dir.assets.source + '/**', ['assets', 'bower-assets']);
+    gulp.watch(dir.assets.bower + '/**', ['assets', 'bower-assets']);
 });
