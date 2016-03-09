91d3510a23754eaf7188f366c0c660b739d5334c
Mahan <eng.mahan.hazrati@gmail.com>
2016-03-09 12:53:39 +0700

    aws

---
+++ config/aws.js
@@ -0,0 +1,10 @@
+module.extends = {
+    s3: {
+        dev: {
+            // don't try to take advantage of it, please
+            // this credentials are test and all the files will remove automaticly after 7 days
+            accessKeyId: 'AKIAJZ3VKGRIH64WUCNA',
+            secretAccessKey: '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'
+        }
+    }
+};
