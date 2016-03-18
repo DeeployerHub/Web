b7e9757627081b50b8ef01fee668be079d878a06
Mahan <eng.mahan.hazrati@gmail.com>
2016-03-18 16:15:15 +0700

    sharing prototype

---
+++ assets/js/app/account/activation/sharing.js
@@ -0,0 +1,10 @@
+(function(angular) {
+    'use strict';
+
+    angular.module("deeployer", [])
+    .controller("SharingController", [
+        '$scope', '$http', 
+        function ($scope, $http) {
+        }
+    ]);
+})(window.angular);
