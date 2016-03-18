2b9739c8082659738ebb4c430ca99281781f95fa
Mahan <eng.mahan.hazrati@gmail.com>
2016-03-18 17:30:35 +0700

    facebook sharing

--- assets/js/app/account/activation/sharing.js
+++ assets/js/app/account/activation/sharing.js
@@ -5,6 +5,20 @@
     .controller("SharingController", [
         '$scope', '$http', 
         function ($scope, $http) {
+            $scope.facebookSharing = function () {
+                FB.ui({
+                    method: 'share',
+                    href: 'http://deeployer.com'
+                }, function(response) {
+                    if (typeof response === 'object') {
+                        $scope.userShared('facebook');
+                    }
+                });
+            };
+
+            $scope.userShared = function (bridge) {
+                // TODO: send request to server to activate the user
+            };
         }
     ]);
 })(window.angular);
