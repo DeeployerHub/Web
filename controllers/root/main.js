d374c32b83e0ab64175c898d57f54c477afec592
Mahan <eng.mahan.hazrati@gmail.com>
2016-04-03 18:06:40 +0700

    add static pages

--- controllers/root/main.js
+++ controllers/root/main.js
@@ -5,5 +5,22 @@ module.exports = {
         res.render('root/pages/landing-page', {
             message : "Hello World"
         });
+    },
+    static: {
+        support: function(req, res) {
+            'use strict';
+
+            res.render('root/pages/statics/support', {});
+        },
+        servicesFeatures: function(req, res) {
+            'use strict';
+
+            res.render('root/pages/statics/services-features', {});
+        },
+        termsAndConditions: function(req, res) {
+            'use strict';
+
+            res.render('root/pages/statics/terms-and-conditions', {});
+        }
     }
 };
--- routes/root/routes.js
+++ routes/root/routes.js
@@ -7,5 +7,10 @@ module.exports = function() {
     // route to specified controllers
     router.get('/', getMiddleware('account.consoleCheck'), controller.landingPage);
 
+    // static pages
+    router.get('/support', controller.static.support);
+    router.get('/services-features', controller.static.servicesFeatures);
+    router.get('/terms-and-conditions', controller.static.termsAndConditions);
+
     return router;
 };
