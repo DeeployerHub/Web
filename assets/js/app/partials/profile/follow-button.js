03c034650174170254c36f5ce1ebefaef090f4f1
Mahan <eng.mahan.hazrati@gmail.com>
2016-04-18 00:39:18 +0700

    add follow button

---
+++ assets/js/app/partials/profile/follow-button.js
@@ -0,0 +1,11 @@
+(function(angular) {
+    'use strict';
+
+    angular.module('deeployer')
+    .controller('PartialsProfileFollowButtonController', [
+        '$scope', '$http', 
+        function ($scope, $http) {
+            
+        }
+    ]);
+})(window.angular);
