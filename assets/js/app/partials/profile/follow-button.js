0c2d041313d2a16118d0fd8190ca28315dd07d60
Mahan <eng.mahan.hazrati@gmail.com>
2016-04-19 18:41:21 +0700

    follow functionalities on front-end

--- assets/js/app/partials/profile/follow-button.js
+++ assets/js/app/partials/profile/follow-button.js
@@ -5,12 +5,46 @@
     .controller('PartialsProfileFollowButtonController', [
         '$scope', '$http', 
         function ($scope, $http) {
-            $scope.followUser = function () {
+            $scope.followUser = function (requestedUsername) {
                 $scope.status='following';
+
+                $scope.request({
+                    'action': 'follow',
+                    'user'  : requestedUsername
+                }, function () {
+                    $scope.status='following';
+                }, function () {
+                    $scope.status='not-following';
+                });
             };
 
-            $scope.unFollowUser = function () {
+            $scope.unFollowUser = function (requestedUsername) {
                 $scope.status='not-following';
+
+                $scope.request({
+                    'action': 'unfollow',
+                    'user'  : requestedUsername
+                }, function () {
+                    $scope.status='not-following';
+                }, function () {
+                    $scope.status='following';
+                });
+            };
+
+            $scope.request = function (data, ok, fail) {
+                $http({
+                    method: 'POST',
+                    url: '/profile/relation',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    data: $.param(data)
+                })
+                .then(function(result){
+                    ok(result);
+                }, function(result){
+                    fail();
+                });
             };
         }
     ]);
