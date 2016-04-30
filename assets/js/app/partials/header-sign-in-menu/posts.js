b86cddb881c8fe48fe24cefcbbdb60c9d3dbe4e3
Mahan <eng.mahan.hazrati@gmail.com>
2016-04-30 14:47:10 +0700

    post js

--- assets/js/app/partials/header-sign-in-menu/posts.js
+++ assets/js/app/partials/header-sign-in-menu/posts.js
@@ -5,6 +5,7 @@
         .controller("HeaderSignInMenuPostsController", [
             '$scope', '$http',
             function ($scope, $http) {
+                $scope.content = '';
                 $scope.maxContentLength = 250;
                 $scope.newPostModal = $('#header-fix-signed-in-new-post-modal');
 
@@ -12,7 +13,38 @@
                     $scope.newPostModal.modal({
                         backdrop: 'static'
                     });
-                }
+                };
+
+                $scope.sendPost = function () {
+                    if (
+                        (maxContentLength - content.length) >= 0 &&
+                        (maxContentLength - content.length) < maxContentLength
+                    ) {
+                        $scope.request({
+                            content: $scope.content
+                        }, function () {
+                            
+                        }, function () {
+
+                        });
+                    }
+                };
+
+                $scope.request = function (data, ok, fail) {
+                    $http({
+                        method: 'POST',
+                        url: '/profile/post',
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        },
+                        data: $.param(data)
+                    })
+                    .then(function(result){
+                        ok(result);
+                    }, function(result){
+                        fail();
+                    });
+                };
             }
         ]);
 })(window.angular);
